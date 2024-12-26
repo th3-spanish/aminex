e27d6e03abe2eee570775fb978279383aaf3d15d
th3-spanish <vevomen1@gmail.com>
2024-12-26 17:11:15 +0100

    changed name to Amine X

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className="pl-0 pr-4">
         <div className="text-xl mb-6"><Code size={32} strokeWidth={2.5}/></div>
         <h1 className="text-[15px] text-gray-300 mb-4 max-w-md leading-relaxed">
-          I'm <span className='text-white font-bold'>Amine Bachane</span>, a Full-Stack Engineer, Problem Solver, and Creator.
+          I'm <span className='text-white font-bold'>Amine X</span>, a Full-Stack Engineer, Problem Solver, and Creator.
         </h1>
         <p className="text-[15px] text-gray-300 mb-6 max-w-md leading-relaxed">
           As a Developer, I have built <span className='text-white font-bold' >+10 products</span>, and continue to craft scalable applications and automate workflows to solve real-world problems. ✌️
